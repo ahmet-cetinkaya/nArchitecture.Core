b822e20fbab594d389e5a39093e173bef373aa64
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-02-08 19:41:55 +0300

    refactor(Core.Application): remove UserForLoginDto and UserForRegisterDto, rename PageRequest to IPageableRequest, and introduce PageableResponse

--- src/Core.Application/Requests/IPageableRequest.cs
+++ src/Core.Application/Requests/IPageableRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace NArchitecture.Core.Application.Requests;
 
-public class PageRequest
+public interface IPageableRequest
 {
     public int PageIndex { get; set; }
     public int PageSize { get; set; }
