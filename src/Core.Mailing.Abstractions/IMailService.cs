20d98cebe1c60a99b692e1099175180d113a2f7c
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-02-10 18:36:12 +0300

    refactor(Core.Mailing): restructure mailing services and introduce abstractions

---
+++ src/Core.Mailing.Abstractions/IMailService.cs
@@ -0,0 +1,15 @@
+﻿namespace NArchitecture.Core.Mailing.Abstractions;
+
+/// <summary>
+/// Defines the contract for sending mail asynchronously.
+/// </summary>
+public interface IMailService
+{
+    /// <summary>
+    /// Sends an email asynchronously.
+    /// </summary>
+    /// <param name="mail">The mail details.</param>
+    /// <param name="cancellationToken">The token to monitor for cancellation requests.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
+    Task SendEmailAsync(Mail mail, CancellationToken cancellationToken = default);
+}
