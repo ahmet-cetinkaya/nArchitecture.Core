5e0ce1bcff586b2d08fcaf712597898c45402c88
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-02-27 23:37:02 +0300

    refactor(Application): remove unused IDto and IResponse interfaces; add documentation for IBusinessRules

--- src/Application/Rules/IBusinessRules.cs
+++ src/Application/Rules/IBusinessRules.cs
@@ -1,3 +1,7 @@
 ﻿namespace NArchitecture.Core.Application.Rules;
 
+/// <summary>
+/// Marker interface for business rules classes.
+/// Classes implementing this interface will be automatically registered with DI container.
+/// </summary>
 public interface IBusinessRules { }
