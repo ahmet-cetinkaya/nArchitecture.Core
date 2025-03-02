83ffb30dd5102eac021b9121f49d8ecdf282ef80
Ahmet Çetinkaya <ahmetcetinkaya@tutamail.com>
2025-03-02 21:24:58 +0300

    refactor(Security): rename GroupId to UserGroupId in UserInGroup class

--- src/Security.Abstractions/Authorization/Entities/UserInGroup.cs
+++ src/Security.Abstractions/Authorization/Entities/UserInGroup.cs
@@ -15,19 +15,19 @@ public class UserInGroup<
 > : BaseEntity<TId>
 {
     public required TUserId UserId { get; set; }
-    public required TUserGroupId GroupId { get; set; }
+    public required TUserGroupId UserGroupId { get; set; }
 
     [Obsolete("This constructor is for ORM, mapper etc.. Do not use it in the code.", true)]
     public UserInGroup()
     {
         UserId = default!;
-        GroupId = default!;
+        UserGroupId = default!;
     }
 
-    public UserInGroup(TUserId userId, TUserGroupId groupId)
+    public UserInGroup(TUserId userId, TUserGroupId userGroupId)
     {
         UserId = userId;
-        GroupId = groupId;
+        UserGroupId = userGroupId;
     }
 
     public virtual User<
